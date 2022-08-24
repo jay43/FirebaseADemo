c742dc84888702e0439a2c41e71e8cc955b5e9be
Prayag Gediya <prayaggediya60@gmail.com>
2022-08-24 13:49:59 +0530

    fcm complete

---
+++ app/src/main/java/com/learn/firebaseademo/MyFirebaseMsgService.kt
@@ -0,0 +1,68 @@
+package com.learn.firebaseademo
+
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+class MyFirebaseMsgService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+        if (remoteMessage.notification != null) {
+            // Since the notification is received directly from
+            // FCM, the title and the body can be fetched
+            // directly as below.
+            showNotification(
+                remoteMessage.notification!!.title!!,
+                remoteMessage.notification!!.body!!
+            )
+        }
+
+    }
+
+    private fun showNotification(title: String, message: String) {
+        // Pass the intent to switch to the MainActivity
+        val intent = Intent(this, MainActivity::class.java)
+        // Assign channel ID
+        val channel_id = "notification_channel"
+        // Here FLAG_ACTIVITY_CLEAR_TOP flag is set to clear
+        // the activities present in the activity stack,
+        // on the top of the Activity that is to be launched
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        // Pass the intent to PendingIntent to start the
+        // next Activity
+        val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_MUTABLE)
+        } else {
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        }
+        val builder: NotificationCompat.Builder = NotificationCompat.Builder(
+            applicationContext,
+            channel_id
+        )
+            .setContentTitle(title)
+            .setContentText(message)
+            .setSmallIcon(R.drawable.ic_broken_image_24)
+            .setAutoCancel(true)
+            .setVibrate(
+                longArrayOf(
+                    1000, 1000, 1000,
+                    1000, 1000
+                )
+            )
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+
+        val manager: NotificationManagerCompat = NotificationManagerCompat.from(this)
+        manager.notify(999, builder.build())
+
+    }
+
+
+}
